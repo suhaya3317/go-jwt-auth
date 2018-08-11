0f9ed2656addfa0478b54839e4ad92d910698aae
suhaya3317 <hayato_17n1200071@nnn.ed.jp>
2018-08-12 01:22:00 +0900

    Initial Commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"jwt-auth/auth"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type post struct {
+	Title string `json:"title"`
+	Tag   string `json:"tag"`
+	URL   string `json:"url"`
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.Handle("/public", public)
+	r.Handle("/private", auth.JwtMiddleware.Handler(private))
+	r.Handle("/auth", auth.GetTokenHandler)
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatalf("ListenAndServe:", nil)
+	}
+}
+
+var public = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	post := &post{
+		Title: "VueCLIからVue.js入門①【VueCLIで出てくるファイルを概要図で理解】",
+		Tag:   "Vue.js",
+		URL:   "https://qiita.com/po3rin/items/3968f825f3c86f9c4e21",
+	}
+	json.NewEncoder(w).Encode(post)
+})
+
+var private = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	post := &post{
+		Title: "VGolangとGoogle Cloud Vision APIで画像から文字認識するCLIを速攻で作る",
+		Tag:   "Go",
+		URL:   "https://qiita.com/po3rin/items/bf439424e38757c1e69b",
+	}
+	json.NewEncoder(w).Encode(post)
+})
